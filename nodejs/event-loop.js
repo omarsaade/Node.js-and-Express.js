75ca4b03ab0befd2cc7acba6e6a822016b567810
omar saadeh <omarsaadeh11@gmail.com>
2022-10-30 11:51:29 -0700

    Node js

--- nodejs/event-loop.js
+++ nodejs/event-loop.js
@@ -1,11 +1,36 @@
 // kif bi shagel el while loop ?
 while (condition) {
   // tick
-}
-
-// Event Loop
-/*
+  // Event Loop
+  /*
 is there any running process ?
 is there any callbacks waiting ?
+
+*/
+}
+
+// bas ye5las bya3mul run lal code li hon maslan
+// w bya3mul end
+end();
+
+/*
+
+NOde js shagale bi tari2et single thread
+w hye asynchrounus . bas hal el nodejs hye 
+asynchronous tameman ? 100 bel 100 ?
+       No
+       hye el node js single thread bas fi umur
+  shagale fia hye multiple thread
+  metel shu ?
+
+node js hye runtime enviroment mabni 3al v8
+
+NodeJs hye 3ibara 3an:
+
+V8
+
+
+library called " Libuv 
+
 
 */
