f6245ad4d078a0d12c5ad2c5c87942e58af5e98b
omar saadeh <omarsaadeh11@gmail.com>
2022-10-31 11:59:11 -0700

    Node js

--- nodejs/event-loop.js
+++ nodejs/event-loop.js
@@ -1,17 +1,40 @@
+// how to run ? node event-loop.js
+
+// heda module men el lebuv..w huwe khaso bi tashfir el
+// password w huwe multiple thread
+const crypto = require("crypto");
+//juwe heda el module 3ana function esmo pbkdf2
+// el function hayde hye li betshafer
+const pbkdf2 = crypto.pbkdf2;
+// deyman fi fare2 bel wa2et ben el multiple thread and
+//single thread.
+const start = Date.now();
+pbkdf2("pass", "salt", 10000, 30, "sha512", () => {
+  console.log("1: ", Date.now() - start);
+});
+
+pbkdf2("pass", "salt", 10000, 30, "sha512", () => {
+  console.log("2: ", Date.now() - start);
+});
+
+// jaweb
+//   2 : 13
+//   1: 21
+
 // kif bi shagel el while loop ?
-while (condition) {
-  // tick
-  // Event Loop
-  /*
+// while (condition) {
+// tick
+// Event Loop
+/*
 is there any running process ?
 is there any callbacks waiting ?
 
 */
-}
+// }
 
 // bas ye5las bya3mul run lal code li hon maslan
 // w bya3mul end
-end();
+// end();
 
 /*
 
@@ -32,7 +55,8 @@ V8
 
 library called " Libuv 
 code tab3 libuv maktub bi c++
-
+v8 shagal single thread and asynchronous
+libuv sheggal multiple thread w huwe synchronous.
 
 
 */
