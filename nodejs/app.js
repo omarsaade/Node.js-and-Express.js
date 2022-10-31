a4bea6024c17b45ea8b36621573a42a9ff24b72a
omar saadeh <omarsaadeh11@gmail.com>
2022-10-31 13:57:32 -0700

    Node js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,3 +1,5 @@
+/*
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
@@ -8,4 +10,21 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
   console.log("server is listening on port 3000");
+});
+
+*/
+
+const express = require("express");
+
+const app = express();
+
+//middleware
+// huwe 3ibara 3an function btsir ma3 kell request byejile
+app.use((req, res, next) => {
+  // hye e5tisar la res.write w res.end bi zet el wa2et
+  res.send("<h1>Hello</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("server Listen on board 3000");
 });
--- nodejs/event-loop.js
+++ nodejs/event-loop.js
@@ -1,62 +1,27 @@
-// how to run ? node event-loop.js
+// asynchronous
+// bya3te nafes el wa2et la, kell
 
-// heda module men el lebuv..w huwe khaso bi tashfir el
-// password w huwe multiple thread
-const crypto = require("crypto");
-//juwe heda el module 3ana function esmo pbkdf2
-// el function hayde hye li betshafer
-const pbkdf2 = crypto.pbkdf2;
-// deyman fi fare2 bel wa2et ben el multiple thread and
-//single thread.
+const request = require("http").request;
+
 const start = Date.now();
-pbkdf2("pass", "salt", 10000, 30, "sha512", () => {
-  console.log("1: ", Date.now() - start);
-});
 
-pbkdf2("pass", "salt", 10000, 30, "sha512", () => {
-  console.log("2: ", Date.now() - start);
-});
+request("http://localhost:5000", (res) => {
+  res.on("data", () => {});
+  res.on("end", () => {
+    console.log(Date.now() - start);
+  });
+}).end();
 
-// jaweb
-//   2 : 13
-//   1: 21
+request("http://localhost:5000", (res) => {
+  res.on("data", () => {});
+  res.on("end", () => {
+    console.log(Date.now() - start);
+  });
+}).end();
 
-// kif bi shagel el while loop ?
-// while (condition) {
-// tick
-// Event Loop
-/*
-is there any running process ?
-is there any callbacks waiting ?
-
-*/
-// }
-
-// bas ye5las bya3mul run lal code li hon maslan
-// w bya3mul end
-// end();
-
-/*
-
-NOde js shagale bi tari2et single thread
-w hye asynchrounus . bas hal el nodejs hye 
-asynchronous tameman ? 100 bel 100 ?
-       No
-       hye el node js single thread bas fi umur
-  shagale fia hye multiple thread
-  metel shu ?
-
-node js hye runtime environment mabni 3al v8
-
-NodeJs hye 3ibara 3an:
-
-V8
-
-
-library called " Libuv 
-code tab3 libuv maktub bi c++
-v8 shagal single thread and asynchronous
-libuv sheggal multiple thread w huwe synchronous.
-
-
-*/
+request("http://localhost:5000", (res) => {
+  res.on("data", () => {});
+  res.on("end", () => {
+    console.log(Date.now() - start);
+  });
+}).end();
