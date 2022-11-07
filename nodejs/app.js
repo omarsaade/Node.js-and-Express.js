276182aee0f7479289e4fe8c9458d42ded8ae813
omar saadeh <omarsaadeh11@gmail.com>
2022-11-07 13:17:39 -0800

    express js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,36 @@
+// with express
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// const bodyParserMW = bodyParser(); //deprecated
+const bodyParserMW = bodyParser.urlencoded({ extended: true });
+//or
+// const bodyParserMW = express.urlencoded({ extended: true });
+
+app.get("/", (req, res, next) => {
+  res.setHeader("Content-Type", "text/html");
+  res.write('<form action="/" method="POST">');
+  res.write('<input name="username"/>');
+  res.write('<input name="age" type="number" />');
+  res.write('<input type="submit"/>');
+  res.write("</form>");
+  res.end();
+});
+
+app.post("/", bodyParserMW, (req, res, next) => {
+  console.log(req.body.username);
+  res.send("done");
+});
+
+app.listen(3000, () => console.log("server listen on port 3000"));
+
 // with node js
 
+/*
 const http = require("http");
 // const body = require("body");
 const body = require("body/form");
@@ -22,3 +53,5 @@ http
     }
   })
   .listen(3000, () => console.log("server listen on port 3000"));
+
+  */
