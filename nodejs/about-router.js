7562875cdf5d734fe56a6d969295640aea0d76ba
omar saadeh <omarsaadeh11@gmail.com>
2022-11-07 09:21:30 -0800

    express js

---
+++ nodejs/about-router.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.send("hello from about");
+});
+
+router.get("/me", (req, res, next) => {
+  res.send("hello from me");
+});
+
+router.get("/company", (req, res, next) => {
+  res.send("hello from company");
+});
+
+module.exports = router;
+
+// or
+
+// exports.router = router
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,31 +1,14 @@
-/*
-
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") res.write("Hello from Home");
-  else if (req.url === "/about") res.write("Hello from about 2");
-  res.end();
-});
-
-server.listen(3000, () => {
-  console.log("server is listening on port 3000");
-});
-
-*/
-
 const express = require("express");
 
+const aboutRouter = require("./about-router");
 const app = express();
 
-//middleware
-// huwe 3ibara 3an function btsir ma3 kell request byejile
-app.use((req, res, next) => {
-  // hye e5tisar la res.write w res.end bi zet el wa2et
-  // yaane 2 bi 1
-  res.send("<h1>Hello</h1>");
+app.get("/", (req, res, next) => {
+  res.send("hello from home");
 });
 
+app.use("/about", aboutRouter);
+
 app.listen(3000, () => {
-  console.log("server Listen on board 3000");
+  console.log("server Listen on port 3000");
 });
