de1a808a393d8e1c80a6315fc63025f5d2b58a88
omar saadeh <omarsaadeh11@gmail.com>
2022-11-08 11:36:31 -0800

    express js

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,9 +7,25 @@ const bodyParser = require("body-parser");
 const app = express();
 
 // const bodyParserMW = bodyParser(); //deprecated
+//form : urlencoded
+// or
+//json :json
+// ana hon btejine data mshafra...so lezem y7awela la data 3adye
+//yaane bado yfek el tashfir men 5ilel library ,
+// awal maktabe esma query string w hye built in bel node js
+//  // const q = require('querystring'); w tene maktabse esma qs
+// iza imet el extended hye true , yaane enta 3am t2elo lal body
+// sta3mel el qs..hye shi mawjud 3al npm
+//law false yaane sta5dam querystring
+//extended lal tashfir iza qs() het true
+// true...cz qs is more safer
+//hye by default true
 const bodyParserMW = bodyParser.urlencoded({ extended: true });
 //or
 // const bodyParserMW = express.urlencoded({ extended: true });
+
+// hayda el middleware lah yetnafaz lama yeje post request
+// npm install --save body-parser
 
 app.get("/", (req, res, next) => {
   res.setHeader("Content-Type", "text/html");
@@ -20,7 +36,7 @@ app.get("/", (req, res, next) => {
   res.write("</form>");
   res.end();
 });
-
+// hayda el middleware lah yetnafaz lama yeje get request
 app.post("/", bodyParserMW, (req, res, next) => {
   // console.log(req.body.username);
   console.log(req.body);
