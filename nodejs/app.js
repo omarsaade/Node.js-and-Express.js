bc6938d61d136465c19c06db1f50172c14d807ab
omar saadeh <omarsaadeh11@gmail.com>
2022-11-09 12:45:26 -0800

    express js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,26 +1,23 @@
 // with express
 
 const express = require("express");
-//he hallet meshklet el operating system taba3na w kif nle2i
-// he hye btle2i la hala w bt7et slash aw backslash hasab iza
-// ken linux aw windows aw...
+const bodyParser = require("body-parser");
 const path = require("path");
-
+// const ejs = require("ejs");
 const app = express();
-//mihsen ta7mil malaf css aw javascript aw sura
-//el server byeje ydawer jawa el static 3al path
+
+app.set("view engine", "ejs");
+app.set("views", "views"); // default 'views'
+
 app.use(express.static(path.join(__dirname, "statics")));
 
 app.get("/", (req, res, next) => {
-  //mnesta5dem shi esmo send file mishen neb3at file html
-  // hye bte5od mene el path taba3 el file w men baada bteb3at el file
-  //heda lal browser
-  //__dirname : btjeble masar el folder men el awal, saret el ossa dynamic
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-  // process.platform === "aix...."  he n7alet bel sater 5
-  // men 5ilel este5dem path
+  res.render("index");
 });
 
+app.post("/", bodyParser.urlencoded({ extended: true }), (req, res, next) => {
+  res.render("index", {
+    name: req.body.name,
+  });
+});
 app.listen(3000, () => console.log("server listen on port 3000"));
-
-// C:\Users\10User\Documents\Node.js-and-Express.js\nodejs
